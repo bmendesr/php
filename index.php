17b97ffe7e364814c47ec179243189f81f3ba78d
bmendesr <bm.ramos@hotmail.com>
2021-06-06 23:11:04 -0300

    lista usuario

--- index.php
+++ index.php
@@ -2,10 +2,28 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyid(3);
 
-echo $root;
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadbyid(3);
+//echo $root;
 
+//Carrega lista de usuarios
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma Lista de usuários buscando pelo login
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+
+$usuario = new Usuario();
+$usuario->login("root","123ç");
+
+echo $usuario;
  ?>
